f8065393c11ddf804371e46db09edfb6bc68d82b
amyasnikov <aleksei.miasnikov@bg.market>
2020-07-02 20:39:56 +0300

    imagico downloader

--- imagico.go
+++ imagico.go
@@ -65,6 +65,29 @@ func search(ll LatLng) (string, error) {
 	return parse(r.Body)
 }
 
+func moveHgt(sourcePath, destPath string) error {
+	inputFile, err := os.Open(sourcePath)
+	if err != nil {
+		return fmt.Errorf("Couldn't open source file: %s", err)
+	}
+	outputFile, err := os.Create(destPath)
+	if err != nil {
+		inputFile.Close()
+		return fmt.Errorf("Couldn't open dest file: %s", err)
+	}
+	defer outputFile.Close()
+	_, err = io.Copy(outputFile, inputFile)
+	inputFile.Close()
+	if err != nil {
+		return fmt.Errorf("Writing to output file failed: %s", err)
+	}
+	err = os.Remove(sourcePath)
+	if err != nil {
+		return fmt.Errorf("Failed removing original file: %s", err)
+	}
+	return nil
+}
+
 func download(tileDir, key string, ll LatLng) (string, error) {
 	url, err := search(ll)
 	if err != nil {
@@ -116,7 +139,7 @@ func download(tileDir, key string, ll LatLng) (string, error) {
 				_, file := path.Split(extractedFilePath)
 				hgt := path.Join(tileDir, file)
 				extracted = append(extracted, hgt)
-				if err := os.Rename(extractedFilePath, hgt); err != nil {
+				if err := moveHgt(extractedFilePath, hgt); err != nil {
 					fmt.Println("move: ", err)
 				}
 			}
